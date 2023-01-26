ee62eec892d72ccced4238cbfc7879d56dd25dea
Andrey <61059883+kaallaash@users.noreply.github.com>
2023-01-26 11:13:24 +0100

    update userEntity and add migration

--- DescriptiveGeometry/Services/AuthorizationService/AuthorizationService.DAL/Entities/UserEntity.cs
+++ DescriptiveGeometry/Services/AuthorizationService/AuthorizationService.DAL/Entities/UserEntity.cs
@@ -8,5 +8,6 @@ public class UserEntity : BaseEntity
     public string Name { get; set; }
     public string Email { get; set; }
     public string Password { get; set; }
+    public string? RefreshToken { get; set; }
     public Role Role { get; set; }
 }
