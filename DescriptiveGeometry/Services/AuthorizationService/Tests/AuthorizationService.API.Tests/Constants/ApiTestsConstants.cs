fde75ed58a9d6252f9506286fb9e61bc7909cd23
Andrey <61059883+kaallaash@users.noreply.github.com>
2023-01-26 16:03:05 +0100

    add tests

--- DescriptiveGeometry/Services/AuthorizationService/Tests/AuthorizationService.API.Tests/Constants/ApiTestsConstants.cs
+++ DescriptiveGeometry/Services/AuthorizationService/Tests/AuthorizationService.API.Tests/Constants/ApiTestsConstants.cs
@@ -4,4 +4,5 @@ public static class ApiTestsConstants
 {
     public const string UserPath = "/api/user";
     public const string TokenPath = "/api/token";
+    public const string RefreshTokenPath = "/api/token/refresh-token";
 }
--- DescriptiveGeometry/Services/AuthorizationService/Tests/AuthorizationService.API.Tests/TokenControllerTests.cs
+++ DescriptiveGeometry/Services/AuthorizationService/Tests/AuthorizationService.API.Tests/TokenControllerTests.cs
@@ -6,13 +6,40 @@ using Xunit;
 using static AuthorizationService.API.Tests.Constants.ApiTestsConstants;
 using static AuthorizationService.API.Tests.Helpers.ClientBuilder;
 using static AuthorizationService.API.Tests.Entities.TestUserEntity;
+using System.Net.Http.Json;
+using AuthorizationService.API.Models;
 
 namespace AuthorizationService.API.Tests;
 
 public class TokenControllerTests
 {
     [Fact]
-    public async Task Post_ValidLoginViewModel_ReturnsToken()
+    public async Task Login_ValidLoginViewModel_ReturnsToken()
+    {
+        await using var application = new AuthorizationApi();
+        var client = await CreateClient(application);
+
+        foreach (var validUserEntity in GetValidUserEntities)
+        {
+            var validLoginViewModel = new LoginViewModel()
+            {
+                Email = validUserEntity.Email,
+                Password = validUserEntity.Password
+            };
+            
+            var jsonValidLoginViewModel = JsonConvert.SerializeObject(validLoginViewModel);
+            var userViewModelContent = new StringContent(jsonValidLoginViewModel, Encoding.UTF8, "application/json");
+            var tokenResponse = await client.PostAsync(TokenPath, userViewModelContent);
+
+            var token = await tokenResponse.Content.ReadFromJsonAsync<TokenModel>();
+
+            Assert.Equal(HttpStatusCode.OK, tokenResponse.StatusCode);
+            Assert.NotNull(token);
+        }
+    }
+
+    [Fact]
+    public async Task RefreshToken_ValidTokenModel_ReturnsToken()
     {
         await using var application = new AuthorizationApi();
         var client = await CreateClient(application);
@@ -26,10 +53,18 @@ public class TokenControllerTests
             };
 
             var jsonValidLoginViewModel = JsonConvert.SerializeObject(validLoginViewModel);
-            var contentUserViewModel = new StringContent(jsonValidLoginViewModel, Encoding.UTF8, "application/json");
+            var userViewModelContent = new StringContent(jsonValidLoginViewModel, Encoding.UTF8, "application/json");
+            var tokenResponse = await client.PostAsync(TokenPath, userViewModelContent);
+            var token = await tokenResponse.Content.ReadFromJsonAsync<TokenModel>();
 
-            var response = await client.PostAsync(TokenPath, contentUserViewModel);
-            Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+            var jsonTokenModel = JsonConvert.SerializeObject(token);
+            var tokenContent = new StringContent(jsonTokenModel, Encoding.UTF8, "application/json");
+
+            var refreshTokenResponse = await client.PostAsync(RefreshTokenPath, tokenContent);
+            var refreshToken = await refreshTokenResponse.Content.ReadFromJsonAsync<TokenModel>();
+
+            Assert.Equal(HttpStatusCode.OK, refreshTokenResponse.StatusCode);
+            Assert.NotNull(refreshToken);
         }
     }
 }
